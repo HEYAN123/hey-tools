5f7edc27b45695ea983257a6ef2460aa50bf439a
贺妍 <heyan139@MacBook-Pro-3.local>
2020-08-06 20:02:31 +0800

    修复mocha插件与node8语法不兼容问题

--- test/test.js
+++ test/test.js
@@ -2,17 +2,17 @@
  * @Author: heyan 
  * @Date: 2020-08-06 10:43:50 
  * @Last Modified by: heyan
- * @Last Modified time: 2020-08-06 17:31:05
+ * @Last Modified time: 2020-08-06 19:24:56
  */
 
 
 'use strict';
 const expect = require('chai').expect;
-// const parseMoneyOfFen = require('../dist/index').parseMoneyOfFen;
+const parseMoneyOfFen = require('../dist/index').parseMoneyOfFen;
 
-// describe('funcs_parseMoneyOfFen', () => {
-//   it('应该输出千分位分割和两位小数的字符串', () => {
-//     const result = parseMoneyOfFen(12345, '--');
-//     expect(result).to.equal('123.45');
-//   });
-// });
+describe('funcs_parseMoneyOfFen', () => {
+  it('应该输出千分位分割和两位小数的字符串', () => {
+    const result = parseMoneyOfFen(12345, '--');
+    expect(result).to.equal('123.45');
+  });
+});
